567cae43096785d399d547b3cbbe49ae9ab0a54f
shuzhen <shu.zhen@4lo.me>
2019-03-01 17:15:50 +0800

    1

---
+++ src/main/java/com/mzfk/entity/Brands.java
@@ -0,0 +1,32 @@
+package com.mzfk.entity;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+/**
+ * @ClassName: Brands
+ * @Description: 牌子
+ * @Author sz
+ * @Date 2019/3/1/16:51
+ */
+@Data
+public class Brands {
+
+    private String code;
+
+    private int name;
+
+    private String wallet;
+
+    private String jackpotUrl;
+
+    private String status;
+
+    private String rate;
+
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
+
+}
---
+++ src/main/java/com/mzfk/mapper/BrandsMapper.java
@@ -0,0 +1,14 @@
+package com.mzfk.mapper;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @ClassName: BrandsMapper
+ * @Description: TODO
+ * @Author sz
+ * @Date 2019/3/1/17:05
+ */
+@Repository
+public interface BrandsMapper {
+
+}
